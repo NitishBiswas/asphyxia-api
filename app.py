fea79ddd3a6ea16a56f41c0bf2caea3bc87beabe
NitishBiswas <nitishbiswas53@gmail.com>
2024-04-13 12:44:04 +0600

    modify app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask_cors import CORS
 import pickle
 import librosa
 import numpy as np
-import requests
-from io import BytesIO
 
 # Load the machine learning model
 model = pickle.load(open("RF_model.pkl", 'rb'))
@@ -18,30 +16,19 @@ CORS(app)  # Enable Cross-Origin Resource Sharing (CORS)
 def home():
     return 'Welcome to the Asphyxia Detection API'
 
-# Define route to handle audio file URL and return prediction
+# Define route to handle audio file and return prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Check if the request contains a URL parameter
-    if 'url' not in request.form:
-        return jsonify({'error': 'No URL parameter found'}), 400
+    # Check if the request contains an audio file
+    if 'audio' not in request.files:
+        return jsonify({'error': 'No audio file found'}), 400
     
-    # Extract the URL parameter from the request
-    audio_url = request.form['url']
-    print('audio_url:', audio_url)
-    # Process the audio file from the URL
+    # Load the audio file from the request
+    audio_file = request.files['audio']
+
+    # Process the audio file
     try:
-        # Download the audio file from the URL
-        response = requests.get(audio_url)
-        
-        # Check if the download was successful
-        if response.status_code != 200:
-            return jsonify({'error': 'Failed to download audio file from URL'}), 500
-        
-        # Load the audio file from the downloaded content
-        audio_content = BytesIO(response.content)
-        audio, sample_rate = librosa.load(audio_content, sr=None)
-        
-        # Extract features from audio
+        audio, sample_rate = librosa.load(audio_file, res_type='kaiser_fast')
         mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
         mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
         mfccs_scaled_features = mfccs_scaled_features.reshape(1, -1)
@@ -54,12 +41,10 @@ def predict():
 
         # Prepare response based on prediction
         result = 'The patient has no Asphyxia' if prediction == 0 else 'The patient has Asphyxia'
-        print('Result:', result)
 
         return jsonify({'result': result, 'prediction': prediction}), 200
     
     except Exception as e:
-        print('Error processing', str(e))
         return jsonify({'error': str(e)}), 500
 
 # Run the Flask app
